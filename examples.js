a8c3ffe189c3feb64c62d403c1dbe0284dce018c
ivanadokic <ivana_pile@yahoo.com>
2021-03-31 22:30:09 -0400

    Update examples.js

--- examples.js
+++ examples.js
@@ -4,3 +4,12 @@ function calcRectArea(width, height) {
 
 console.log(calcRectArea(5, 6));
 //-------> Output: 30
+
+## Use `.map()` to transform an Array
+const array1 = [1, 4, 9, 16];
+
+// pass a function to map
+const map1 = array1.map(x => x * 2);
+
+console.log(map1);
+//-------> Output: [2, 8, 18, 32]
